f55fac71e9a66c41f14ddfc189b78105f58e6c2e
Death Killer <884052+deathkiller@users.noreply.github.com>
2026-04-03 16:34:15 +0200

    Initial commit

---
+++ Casks/jazz2.rb
@@ -0,0 +1,27 @@
+cask "jazz2" do
+  # NOTE: "2" is not a version number, but an intrinsic part of the product name
+  arch arm: "ARM64", intel: "x64"
+
+  version "3.5.0"
+  sha256 "3f19db63e2bdd7b81b90dad94605500be5e11e86168fca99741b0c727211f5b4"
+
+  url "https://github.com/deathkiller/jazz2/releases/download/#{version}/Jazz2_#{version}_MacOS.zip",
+      verified: "github.com/deathkiller/jazz2/"
+  name "Jazz² Resurrection"
+  desc "Open-source reimplementation of Jazz Jackrabbit 2"
+  homepage "https://deat.tk/jazz2/"
+
+  container nested: "#{arch}/jazz2_sdl2.dmg"
+
+  app "Jazz² Resurrection.app"
+
+  postflight do
+    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/Jazz² Resurrection.app"
+  end
+
+  zap trash: "~/Library/Application Support/Jazz² Resurrection"
+
+  caveats <<~EOS
+    Game data should be installed to ~/Library/Application Support/Jazz² Resurrection/Source/
+  EOS
+end
---
+++ README.md
@@ -0,0 +1,18 @@
+<div align="center">
+    <a href="https://github.com/deathkiller/jazz2"><img src="https://raw.githubusercontent.com/deathkiller/jazz2/master/Docs/Logo.gif" alt="Jazz² Resurrection" title="Jazz² Resurrection"></a>
+</div>
+
+<div align="center">
+    Open-source <strong>Jazz Jackrabbit 2</strong> reimplementation
+</div>
+
+<div align="center">
+  <sub>
+    Brought to you by <a href="https://github.com/deathkiller">@deathkiller</a>
+  </sub>
+</div>
+<hr/>
+
+<div align="center">
+    <b>This repository contains only Homebrew tap providing a macOS cask for <a href="https://github.com/deathkiller/jazz2-native">deathkiller/jazz2-native</a>.</b><br>For more information, visit <a href="http://deat.tk/jazz2/">the official website</a>. You can install it using <code>brew install deathkiller/jazz2/jazz2</code>
+</div>
