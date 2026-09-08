8dde4db0de05ab66d71652e6a37e93322339cb31
Kreeblah <kreeblah@gmail.com>
2026-09-08 13:15:26 -0700

    Update cask for current Homebrew standards (#2)

--- Casks/jazz2.rb
+++ Casks/jazz2.rb
@@ -5,18 +5,18 @@ cask "jazz2" do
   version "3.8.0"
   sha256 "5a051dab4260b1bd99d2a9285242a6b062c6b5f88dba773c98b4dd786a42bab7"
 
-  url "https://github.com/deathkiller/jazz2/releases/download/#{version}/Jazz2_#{version}_MacOS.zip",
-      verified: "github.com/deathkiller/jazz2/"
+  url "https://github.com/deathkiller/jazz2/releases/download/#{version}/Jazz2_#{version}_MacOS.zip"
   name "Jazz² Resurrection"
   desc "Open-source reimplementation of Jazz Jackrabbit 2"
   homepage "https://de4th.dev/jazz2/"
 
+  depends_on :macos
   container nested: "#{arch}/jazz2_sdl2.dmg"
 
   app "Jazz² Resurrection.app"
 
-  postflight do
-    system "xattr", "-r", "-d", "com.apple.quarantine", "#{appdir}/Jazz² Resurrection.app"
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-r", "-d", "com.apple.quarantine", "/Applications/Jazz² Resurrection.app"]
   end
 
   zap trash: "~/Library/Application Support/Jazz² Resurrection"
